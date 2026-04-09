4125cd60faf6bc0b142ea2013970ecefb7f21a7a
twmeggett <twmeggett@gmail.com>
2026-04-09 01:56:35 -0400

    add workflow to enable github pages

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/charter-react/',
   plugins: [react(), tailwindcss()],
-
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
